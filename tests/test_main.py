07c4d1a89a5bcd8ffd499ab26e57e7b158b3e4c4
torupa <xpt77@outlook.com>
2023-02-26 19:09:26 +0900

    pytest cover update

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,8 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+def test_read_item():
+    response = client.get('/users/100')
+    assert response.status_code == 200
+    assert response.json() == {'user_id': 100}
